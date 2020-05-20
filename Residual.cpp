2d8d66c2e3801bfeda12f06e6b572bc24809c651
Lia Formenti <lia.frances.formenti@cern.ch>
2020-05-20 17:53:12 +0200

    constructor for Residual class

---
+++ Residual.cpp
@@ -0,0 +1,18 @@
+// Residual object
+#define Residual_cxx
+#include "Residual.h"
+
+using namespace std;
+
+Residual::Residual(Double_t residual, UShort_t layer,
+                   Double_t xpos, Double_t ypos,
+                   UShort_t fixedLayer1, UShort_t fixedLayer2) {
+    // Initialize members
+    res = residual;
+    l = layer;
+    x = xpos;
+    y = ypos;
+    la = fixedLayer1;
+    lb = fixedLayer2;
+    std::cout << res << ' ' << l << ' ' << x << ' ' << y << ' ' << la << ' ' << lb << '\n';
+}
---
+++ Residual.h
@@ -0,0 +1,34 @@
+#ifndef Residual_h
+#define Residual_h
+
+// C++ includes
+#include <iostream>
+
+// Root includes
+#include "Rtypes.h"
+
+// tgc_analysis includes
+
+// My includes
+
+// Class to store residual, layer it is on, position, 
+// and which fixed layers it was calculated with respect to
+
+class Residual {
+  public:
+    // Constructors
+    Residual(){};
+    Residual(Double_t residual, UShort_t layer, 
+             Double_t xpos, Double_t ypos, 
+             UShort_t fixedLayer1, UShort_t fixedLayer2);
+    ~Residual(){};
+
+    // Members
+    Double_t res;
+    UShort_t l;
+    Double_t x;
+    Double_t y;
+    UShort_t la;
+    UShort_t lb;
+};
+#endif
