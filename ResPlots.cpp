c51fde928c10cf6cb3ffc674207bee72fc4c7ea5
Lia Formenti <lia.frances.formenti@cern.ch>
2020-06-18 21:19:46 +0200

    finished initialize num entries plots

--- ResPlots.cpp
+++ ResPlots.cpp
@@ -13,13 +13,19 @@ void ResPlots::CreateNumEntriesTH2Is(string nameBase) {
     InitializeNumEntriesTH2Is(nameBase);
     pm->PrintAll();
 }
+// Note that bin edges vectors of binning are transfered to Double_t
+// arrays for use with PlotManager methods.
 void ResPlots::InitializeNumEntriesTH2Is(string nameBase) {
     string name;
+    Double_t xEdges[binning->nBinsX + 1];
+    copy(binning->xBinEdges.begin(), binning->xBinEdges.end(), xEdges);
+    Double_t yEdges[binning->nBinsY + 1];
+    copy(binning->yBinEdges.begin(), binning->yBinEdges.end(), yEdges);
     vector<Combination> combVec = combinationVector();    
     for (auto combo=combVec.begin(); combo!=combVec.end(); combo++) {
         name = nameBase + combo->String();
-        // pm->Add(name, name, binning->nBinsX, binning->xBinEdges, 
-        //        binning->nBinsY, binning->yBinEdges, myTH2I); 
+        pm->Add(name, name, binning->nBinsX, xEdges,
+               binning->nBinsY, yEdges, myTH2I); 
     }
     return;
 }
