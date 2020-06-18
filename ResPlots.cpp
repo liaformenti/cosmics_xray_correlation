fdf9d35331479902b86992ceaa82bb86c99680d9
Lia Formenti <lia.frances.formenti@cern.ch>
2020-06-18 21:37:13 +0200

    CreateNumEntriesTH1Is done

--- ResPlots.cpp
+++ ResPlots.cpp
@@ -9,10 +9,33 @@ ResPlots::ResPlots(vector<Residual>* _residuals,
                    PlotManager* _pm) : residuals(_residuals),
                    binning(_binning), g(_g), pm(_pm) {}
 
+// Shows number of entries in each bin
+// Plot for each combination
 void ResPlots::CreateNumEntriesTH2Is(string nameBase) {
     InitializeNumEntriesTH2Is(nameBase);
-    pm->PrintAll();
+    string name;
+    Combination combo;
+    for (auto r=residuals->begin(); r!=residuals->end(); r++) {
+        combo = r->GetCombo(); 
+        name = nameBase + combo.String();
+        pm->Fill(name, r->x, r->y);
+    }
+    /*Combination c1(1,3,4);
+    Combination c2(2,3,4);
+    TCanvas* c = new TCanvas("canvas", "canvas");
+    c->Print("test.pdf[");
+    TH2F* l1Hist = (TH2F*) pm->Get(nameBase+c1.String());
+    l1Hist->Draw("Colz");
+    c->Print("test.pdf");
+    c->Clear();
+    TH2F* l2Hist = (TH2F*) pm->Get(nameBase+c2.String());
+    l2Hist->Draw("Colz");
+    c->Print("test.pdf");
+    c->Print("test.pdf]");
+    delete c;*/
+    return;
 }
+
 // Note that bin edges vectors of binning are transfered to Double_t
 // arrays for use with PlotManager methods.
 void ResPlots::InitializeNumEntriesTH2Is(string nameBase) {
