c693e4bc04e7344d0242189976a34cb60923f1fc
Lia Formenti <lia.frances.formenti@cern.ch>
2020-07-07 21:43:01 +0200

    prep to push to production

--- RunAnalysis.cpp
+++ RunAnalysis.cpp
@@ -43,7 +43,7 @@ void RunAnalysis(TTree &trksTree, AnalysisInfo* info, PlotManager* pm, DetectorG
     // Replace i<x=nEntries eventually
     // 3 events ensures you get one that passes cut 
     // with testCA_qs3p7.root with L3 and L4 fixed
-    for (Int_t i=0; i<3; i++) {
+    for (Int_t i=0; i<nEntries; i++) {
         trksTree.GetEntry(i);
         // Uncertainty in x is width of wire group / sqrt(12)
         // Assumes uniform position distribution of hit across group
@@ -53,8 +53,8 @@ void RunAnalysis(TTree &trksTree, AnalysisInfo* info, PlotManager* pm, DetectorG
 
         // for each permutation of two layers
         // la < lb and treated first always
-        // for (Int_t la=1; la<=4; la++) {
-        for (UShort_t la=3; la<=3; la++) {
+        for (Int_t la=1; la<=4; la++) {
+        // for (UShort_t la=3; la<=3; la++) {
             for (Int_t lb=(la+1); lb<=4; lb++) {
             // for (UShort_t lb=(la+1); lb<=4; lb++) {
 
@@ -93,8 +93,7 @@ void RunAnalysis(TTree &trksTree, AnalysisInfo* info, PlotManager* pm, DetectorG
         }*/
     } // end event loop
     // printUncertaintyHistograms(pm);
-    // Get xray data
-    // XRayData("results.db", info);
+    // Example output choices
     /*Binning loose(500, 500, g); // large rectangular bins
     ResPlots loosePlots(&residuals, &loose, loose.name, info, g, pm);
     loosePlots.CreateNumEntriesTH2Is();
@@ -103,6 +102,9 @@ void RunAnalysis(TTree &trksTree, AnalysisInfo* info, PlotManager* pm, DetectorG
     loosePlots.PrintNumEntriesTH2Is("out/test_num_entries.pdf");
     loosePlots.PrintPosBinnedResPlots("out/test_res_plots.pdf");
     loosePlots.PrintPosBinnedFitResultTH2Fs("out/test_results.pdf");*/
+
+    // Get xray data
+    // XRayData("results.db", info);
     return;
 }
 
